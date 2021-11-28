8cbce8ea186b5f82f2cc10bea2e9fe5af73aa18c
abhishek <abhishek.varma.atwork@gmail.com>
2021-11-28 21:56:01 +0530

    added update product functionality

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,8 @@ import AdminDashboard from "./user/AdminDashBoard";
 import AddCategory from "./admin/AddCategory";
 import ManageCategories from "./admin/ManageCategories";
 import AddProduct from "./admin/AddProduct";
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
   return (
@@ -27,6 +29,12 @@ const Routes = () => {
         />
         <AdminRoute path="/admin/category" exact component={ManageCategories} />
         <AdminRoute path="/admin/create/product" exact component={AddProduct} />
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
+        <AdminRoute
+          path="/admin/product/update/:productId"
+          exact
+          component={UpdateProduct}
+        />
       </Switch>
     </BrowserRouter>
   );
